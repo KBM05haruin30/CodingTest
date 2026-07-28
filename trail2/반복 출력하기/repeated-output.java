4d9e43564587bff64a22d180a4e86bc860e99bf0
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-07-28 13:28:43 +0900

    [Passed] 반복 출력하기 (131ms, 10MB)

---
+++ trail2/반복 출력하기/repeated-output.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        rpstr(N);
+    }
+
+    private static void rpstr(int N)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            System.out.println("12345^&*()_");
+        }
+    }
+}
