964baf6bc713089b654b05fb4c2d2fe3b211eb36
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-07-29 10:57:12 +0900

    [Passed] 문자 변경하기 (98ms, 10MB)

---
+++ trail1/문자 변경하기/change-charater.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        char c = 'C';
+        c = 'T';
+        System.out.print(c);
+    }
+}
