d750f9689624b8c2bd25375de5bde308bab04dfd
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-07-29 10:55:06 +0900

    [Passed] 길이 단위 변환하기 (98ms, 9MB)

---
+++ trail1/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        double ft = 30.48;
+        double mi = 160934;
+        double ansft = 9.2;
+        double ansmi = 1.3;
+        System.out.printf("%.1fft = %.1fcm \n%.1fmi = %.1fcm", ansft, (ansft * ft), ansmi, (ansmi * mi));
+    }
+}
