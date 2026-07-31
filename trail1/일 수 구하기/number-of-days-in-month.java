69609b37a702d44befa7c5f7faec7ab14d21f98a
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-07-31 12:16:55 +0900

    [Passed] 일 수 구하기 (134ms, 10MB)

---
+++ trail1/일 수 구하기/number-of-days-in-month.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n == 1 || n == 3 || n == 5 || n == 7 || n == 8 || n == 10 || n == 12)
+        {
+            System.out.println(31);
+        } else if (n == 2)
+        {
+            System.out.println(28);
+        } else
+        {
+            System.out.println(30);
+        }
+    }
+}
