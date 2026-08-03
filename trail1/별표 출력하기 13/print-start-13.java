cc1757f8d273d98ca578ace1099e0f5e0bce1f44
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-03 17:20:47 +0900

    [Passed] 별표 출력하기 13 (137ms, 10MB)

---
+++ trail1/별표 출력하기 13/print-start-13.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 0; i < 2*N; i++)
+        {
+            if (i % 2 == 0)
+            {
+                for (int j = i/2; j < N; j++)
+                {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            } else
+            {
+                for (int j = 0; j < i/2+1; j++)
+                {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
