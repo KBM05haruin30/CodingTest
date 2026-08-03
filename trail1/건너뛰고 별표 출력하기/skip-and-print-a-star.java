ddc8dc85453d33e7353c8cfd639f0a01b098d413
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-03 16:18:20 +0900

    [Passed] 건너뛰고 별표 출력하기 (135ms, 12MB)

---
+++ trail1/건너뛰고 별표 출력하기/skip-and-print-a-star.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int cnt = 1;
+        for (int i = 1; i <= N*2-1; i++)
+        {
+            for (int j = 1; j <= cnt; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n\n");
+            if (i < N)
+            {
+                cnt++;
+            } else
+            {
+                cnt--;
+            }
+        }
+    }
+}
