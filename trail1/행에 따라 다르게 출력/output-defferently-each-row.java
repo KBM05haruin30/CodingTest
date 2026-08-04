970560c4dc107ae1f2d9230cfa773c07dcf3e984
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-04 11:51:37 +0900

    [Passed] 행에 따라 다르게 출력 (139ms, 10MB)

---
+++ trail1/행에 따라 다르게 출력/output-defferently-each-row.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int cnt = 1;
+        for (int i = 1; i <= N; i++)
+        {
+            if (i % 2 != 0)
+            {
+                for (int j = 1; j <= N; j++)
+                {
+                    System.out.print(cnt + " ");
+                    if (j == N)
+                    {
+                        cnt += 2;
+                    } else
+                    {
+                        cnt++;
+                    }
+                }
+            }
+            else
+            {
+                for (int j = 1; j <= N; j++)
+                {
+                    System.out.print(cnt + " ");
+                    if (j == N)
+                    {
+                        cnt++;
+                    } else
+                    {
+                        cnt += 2;
+                    }
+
+                }
+            }
+            System.out.println();
+        }
+    }
+}
