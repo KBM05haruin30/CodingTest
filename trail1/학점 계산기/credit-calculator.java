7e537a937359b462dff133831701ab14dc5e34d3
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-05 08:50:14 +0900

    [Passed] 학점 계산기 (166ms, 12MB)

---
+++ trail1/학점 계산기/credit-calculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        double[] arr = new double[N];
+        double sum = 0;
+        for (int i = 0; i < N; i++)
+        {
+            arr[i] = sc.nextDouble();
+            sum += arr[i];
+        }
+        double avg = sum/N;
+        System.out.printf("%.1f\n", avg);
+        if (avg >= 4.0)
+        {
+            System.out.print("Perfect");
+        } else if (avg >= 3.0)
+        {
+            System.out.print("Good");
+        } else
+        {
+            System.out.print("Poor");
+        }
+    }
+}
