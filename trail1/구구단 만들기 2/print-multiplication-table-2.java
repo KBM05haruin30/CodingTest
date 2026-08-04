39ce5e3cece4e93ca3a78838f975df773dabd0d2
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-04 12:10:24 +0900

    [Passed] 구구단 만들기 2 (125ms, 10MB)

---
+++ trail1/구구단 만들기 2/print-multiplication-table-2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        for(int i = 2; i <= 8; i+=2)
+        {
+            for (int j = B; j >= A; j--)
+            {
+                System.out.print(j + " * " + i + " = " + (i*j));
+                if (j != A)
+                {
+                    System.out.print(" / ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
