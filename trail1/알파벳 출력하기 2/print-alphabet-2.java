341056da87a24feebe334e635484135d74ed78f1
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-04 12:43:56 +0900

    [Passed] 알파벳 출력하기 2 (132ms, 10MB)

---
+++ trail1/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        char c = 'A';
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < N - i; j++)
+            {
+                System.out.print(c + " ");
+                if (c != 'Z')
+                {
+                    c++;
+                } else
+                {
+                    c = 'A';
+                }
+            }
+            System.out.println();
+        }
+    }
+}
