10b3f21f10b2ab989b6319928a5bf76c55c4091e
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-06 07:57:10 +0900

    [Passed] 지그재그로 정수 채우기 (125ms, 10MB)

---
+++ trail1/지그재그로 정수 채우기/zigzag-numbering.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[][] arr = new int[N][M];
+
+        int num = 0;
+        for (int i = 0; i < M; i++)
+        {
+            if (i % 2 == 0)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    arr[j][i] = num;
+                    num++;
+                }    
+            } else
+            {
+                for (int j = N-1; j >= 0; j--)
+                {
+                    arr[j][i] = num;
+                    num++;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < M; j++)
+            {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
