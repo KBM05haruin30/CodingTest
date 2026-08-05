9abc5b7b4d930e834136f70edf9c46bc0bbb4ef2
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-05 11:42:07 +0900

    [Passed] 두 수의 차의 최솟값 (133ms, 10MB)

---
+++ trail1/두 수의 차의 최솟값/minimum-difference-between-two-numbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N-1; i++)
+        {
+            for (int j = 1 + i; j < N; j++)
+            {
+                if (arr[j] - arr[i] < min)
+                {
+                    min = arr[j] - arr[i];
+                }
+            }
+        }
+        System.out.print(min);
+    }
+}
