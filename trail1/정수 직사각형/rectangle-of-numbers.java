2ca6f36468d2078606baf4c58b2a17d8c07ac4e9
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-06 07:44:26 +0900

    [Passed] 정수 직사각형 (132ms, 10MB)

---
+++ trail1/정수 직사각형/rectangle-of-numbers.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[][] arr = new int[N][M];
+        int num = 1;
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < M; j++)
+            {
+                arr[i][j] = num;
+                num++;
+            }
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < M; j++)
+            {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
