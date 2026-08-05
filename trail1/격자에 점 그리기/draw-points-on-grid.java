53f330378646b388daaa58a29feb8b450f10f29a
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-06 08:22:22 +0900

    [Passed] 격자에 점 그리기 (130ms, 10MB)

---
+++ trail1/격자에 점 그리기/draw-points-on-grid.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[][] arr = new int[N][N];
+
+        int cnt = 1;
+        for (int i = 0; i < M; i++)
+        {
+            int r = sc.nextInt();
+            int c = sc.nextInt();
+            arr[r-1][c-1] = cnt;
+            cnt++;
+        }
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
