0640d5ed6ce6c9d35b6fe0a8120f907ab3c0ae28
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-05 10:08:38 +0900

    [Passed] 점수대 파악하기 (126ms, 10MB)

---
+++ trail1/점수대 파악하기/find-out-the-score-range.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[11];
+        for (int i = 0; i <100; i++)
+        {
+            int num = sc.nextInt();
+            if (num == 0)
+            {
+                break;
+            }
+            arr[num/10]++;
+        }
+        for (int i = 10; i > 0; i--)
+        {
+            System.out.println((i*10) + " - " + arr[i]);
+        }
+    }
+}
