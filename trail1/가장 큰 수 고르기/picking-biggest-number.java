326c081b11a70abf80fd4f51670c767ceddd032e
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-05 11:05:06 +0900

    [Passed] 가장 큰 수 고르기 (133ms, 10MB)

---
+++ trail1/가장 큰 수 고르기/picking-biggest-number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        final int INT_MIN = Integer.MIN_VALUE;
+        int max = INT_MIN;
+        for (int i = 0; i < 10; i++)
+        {
+            int num = sc.nextInt();
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        System.out.print(max);
+    }
+}
