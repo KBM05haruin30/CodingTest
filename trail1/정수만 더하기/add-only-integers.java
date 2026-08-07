c5aaa551cc63bdc13874ce1b7dc38f27686bfbb3
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-08 01:45:00 +0900

    [Passed] 정수만 더하기 (119ms, 10MB)

---
+++ trail1/정수만 더하기/add-only-integers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        int sum = 0;
+        for (int i = 0; i < A.length(); i++)
+        {
+            if (A.charAt(i) >= '0' && A.charAt(i) <= '9')
+            {
+                sum += (int)(A.charAt(i)-'0');
+            }
+        }
+        System.out.print(sum);
+    }
+}
