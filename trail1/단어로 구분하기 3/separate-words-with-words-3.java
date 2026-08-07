7bb7311790e6193057aa0f54065fa5587f53f3a3
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-07 11:19:05 +0900

    [Passed] 단어로 구분하기 3 (122ms, 10MB)

---
+++ trail1/단어로 구분하기 3/separate-words-with-words-3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String[] s = new String[10];
+        for (int i = 9; i >= 0; i--)
+        {
+            s[i] = sc.next();
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
