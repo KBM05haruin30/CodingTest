48265f865cf04f18b379160878d46120167fbfd2
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-07 11:21:23 +0900

    [Passed] 문자열 전부 붙이기 (124ms, 10MB)

---
+++ trail1/문자열 전부 붙이기/paste-all-string.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = "";
+        for (int i = 0; i < n; i++)
+        {
+            s += sc.next();
+        }
+        System.out.print(s);
+    }
+}
