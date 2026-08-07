fb77154e7adf4f25ec43b13a451a8dbbde7611b0
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-07 11:57:48 +0900

    [Passed] 부분 문자열의 개수 (130ms, 10MB)

---
+++ trail1/부분 문자열의 개수/number-of-substrings.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        int cnt = 0;
+        for (int i = 0; i < A.length() -1; i++)
+        {
+            if (A.substring(i, i + 2).equals(B))
+            {
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
