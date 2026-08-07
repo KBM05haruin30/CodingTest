7c4c960c7db8f12166bd747e468604c075f3bd9c
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-07 12:07:38 +0900

    [Passed] 두 번째를 첫 번째로 (119ms, 10MB)

---
+++ trail1/두 번째를 첫 번째로/second-to-first.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        char[] arr = s.toCharArray();
+        char c1 = arr[0];
+        char c2 = arr[1];
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (arr[i] == c2)
+            {
+                arr[i] = c1;
+            }
+        }
+        System.out.print(String.valueOf(arr));
+    }
+}
