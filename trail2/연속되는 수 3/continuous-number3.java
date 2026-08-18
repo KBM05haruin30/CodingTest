34e24d1b398e8c9718a24b9e1e48603665c2db4e
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-19 01:30:56 +0900

    [Passed] 연속되는 수 3 (186ms, 20MB)

---
+++ trail2/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        int cnt = 0;
+        int ans = 0;
+        for (int i = 0; i < N; i++)
+        {
+            if (i == 0 || (arr[i] < 0 && arr[i-1] > 0) || (arr[i] > 0 && arr[i-1] < 0))
+            {
+                cnt = 1;
+            } else
+            {
+                cnt++;
+            }
+            ans = Math.max(ans, cnt);
+        }
+
+        System.out.print(ans);
+    }
+}
