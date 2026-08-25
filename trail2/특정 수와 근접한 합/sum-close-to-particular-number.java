48e47f0b8ec5712470b335a3094dba5efdf9d5bb
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-25 11:03:24 +0900

    [Passed] 특정 수와 근접한 합 (146ms, 10MB)

---
+++ trail2/특정 수와 근접한 합/sum-close-to-particular-number.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int s = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        int min_val = Integer.MAX_VALUE;
+        for (int i = 0; i < n-1; i++)
+        {
+            for (int j = i+1; j < n; j++)
+            {
+                int sum = 0;
+                for (int k = 0; k < n; k++)
+                {
+                    if (k == i || k == j)
+                    {
+                        continue;
+                    }
+                    sum += arr[k];
+                }
+                if (min_val > Math.abs(sum - s))
+                {
+                    min_val = Math.abs(sum - s);
+                }
+            }
+        }
+        System.out.print(min_val);
+    }
+}
