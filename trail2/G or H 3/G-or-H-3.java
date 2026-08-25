be27f5be66d90bb8f672eb666c8cf0f7586cc3f8
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-08-26 06:07:16 +0900

    [Passed] G or H 3 (156ms, 10MB)

---
+++ trail2/G or H 3/G-or-H-3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[10001];
+        for (int i = 0; i < n; i++) {
+            int pos = sc.nextInt();
+            char c = sc.next().charAt(0);
+            if (c == 'G')
+            {
+                arr[pos-1] = 1;
+            } else if ( c == 'H')
+            {
+                arr[pos-1] = 2;
+            }
+        }
+        // Please write your code here.
+        int max_val = 0;
+        for (int i = 0; i <= 10000 - k; i++)
+        {
+            int sum = 0;
+            for (int j = i; j <= i+k; j++)
+            {
+                sum += arr[j];
+            }
+            max_val = Math.max(max_val, sum);
+        }
+        System.out.print(max_val);
+    }
+}
