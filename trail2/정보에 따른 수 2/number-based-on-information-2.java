6dfa1854fd4bf58a38a59a5a204c8207364b0402
Haruin <138747411+KBM05haruin30@users.noreply.github.com>
2026-09-09 17:45:57 +0900

    [Passed] 정보에 따른 수 2 (182ms, 11MB)

---
+++ trail2/정보에 따른 수 2/number-based-on-information-2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        char[] c = new char[T];
+        int[] x = new int[T];
+        for (int i = 0; i < T; i++) {
+            c[i] = sc.next().charAt(0);
+            x[i] = sc.nextInt();
+        }
+        // Please write your code here.
+
+        int cnt = 0;
+        for (int k = a; k <= b; k++)
+        {
+            int d1 = Integer.MAX_VALUE;
+            int d2 = Integer.MAX_VALUE;
+            for (int i = 0; i < T; i++)
+            {
+                if (c[i] == 'S')
+                {
+                    d1 = Math.min(d1, Math.abs(k - x[i]));
+                } else if (c[i] == 'N')
+                {
+                    d2 = Math.min(d2, Math.abs(k - x[i]));
+                }
+            }
+            if (d1 <= d2)
+            {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
